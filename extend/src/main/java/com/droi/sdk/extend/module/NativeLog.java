d56369934bf5a94b6609e111a66f08fda18d397c
marekchen <c491843272@gmail.com>
2017-09-11 11:03:32 +0800

    [commit]20170911

---
+++ extend/src/main/java/com/droi/sdk/extend/module/NativeLog.java
@@ -0,0 +1,18 @@
+package com.droi.sdk.extend.module;
+
+import android.util.Log;
+
+import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.common.WXModule;
+
+/**
+ * Created by chenpei on 2017/9/11.
+ */
+
+public class NativeLog extends WXModule {
+
+    @JSMethod()
+    public void printLog(String msg) {
+        Log.e("chenpei", msg);
+    }
+}
